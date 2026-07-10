1293da1b99455f1542821a059e77b4289e9e2ad4
Dimple2906 <167692564+Dimple2906@users.noreply.github.com>
2026-07-11 00:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # -----------------------------
 # User Input
